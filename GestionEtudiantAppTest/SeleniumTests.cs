7950d8e77825f5bb2c971c16ee4b1167381fdff5
juleba580 <lilsoulbah@gmail.com>
2024-04-23 07:00:20 -0400

    Update SeleniumTests.cs
    s

--- GestionEtudiantAppTest/SeleniumTests.cs
+++ GestionEtudiantAppTest/SeleniumTests.cs
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
-﻿using OpenQA.Selenium;
-=======
 using OpenQA.Selenium;
->>>>>>> 11dd71d77425fe89c60438b9959d78ce8e42976a
 using OpenQA.Selenium.Chrome;
 using Xunit;
 
 namespace GestionEtudiantAppTest
 {
-<<<<<<< HEAD
     public class SeleniumTests
     {
         [Fact]
@@ -44,50 +39,6 @@ namespace GestionEtudiantAppTest
 
             // Fermer le navigateur
             driver.Quit();
-=======
-    public class SeleniumTests : IClassFixture<ChromeDriverFixture>
-    {
-        private readonly IWebDriver _driver;
-
-        public SeleniumTests(ChromeDriverFixture fixture)
-        {
-            _driver = fixture.Driver;
-        }
-
-        [Fact]
-        public void VerifyStudentListPageDisplaysCorrectly()
-        {
-            // Naviguer vers la page des étudiants
-            _driver.Navigate().GoToUrl("http1://localhost:7173/Etudiants/Index");
-
-            // Vérifier que la page est chargée en vérifiant le titre ou d'autres éléments distinctifs
-            Assert.Contains("Liste des étudiants", _driver.Title);
-
-            // Vérifier la présence de la table des étudiants
-            IWebElement table = _driver.FindElement(By.ClassName("table-striped"));
-            Assert.NotNull(table);
-
-            // Vérifier que la table contient des données d'étudiants
-            IWebElement tbody = table.FindElement(By.TagName("tbody"));
-            Assert.NotEmpty(tbody.FindElements(By.TagName("tr")));
-        }
-    }
-
-    public class ChromeDriverFixture : IDisposable
-    {
-        public IWebDriver Driver { get; }
-
-        public ChromeDriverFixture()
-        {
-            // Initialiser ChromeDriver
-            Driver = new ChromeDriver();
-        }
-
-        public void Dispose()
-        {
-            // Fermer ChromeDriver
-            Driver.Quit();
->>>>>>> 11dd71d77425fe89c60438b9959d78ce8e42976a
         }
     }
 }
